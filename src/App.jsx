fcf6b3b77e3fd154ccaed2b5e66be3067a2689f9
sabalearning01 <sabatemitayo44@gmail.com>
2024-07-29 12:49:40 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import TypiApi from './Pages/Api/Posts'
+import './App.css'
+import Posts from './Pages/Api/Posts'
+
+function App() {
+  
+
+  return (
+
+      <div>
+        <Posts />
+
+      </div>
+    
+  )
+}
+
+export default App
